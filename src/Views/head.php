98858fcc6dffcfef792f2e831d7aa6e6deed5603
basic-app <dev@basic-app.com>
2024-02-13 22:14:08 +0300

    n/a

--- src/Views/head.php
+++ src/Views/head.php
@@ -4,7 +4,7 @@ use BasicApp\Js\TinyMce\Config\TinyMce;
 ?>
 <script type="text/javascript" src="<?= base_url('assets/tinymce/tinymce.min.js');?>"></script>
 <script type="text/javascript">
-function init_tinymce(selector, language)
+function init_tinymce()
 {
     tinymce.init(TINYMCE_CONFIG);
 }
